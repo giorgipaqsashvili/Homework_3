d187fa63d6ac1bc92da255fdecbedff66bb0461d
giorgipaqsashvili <giorgipaqsashvili3@gmail.com>
2024-10-23 07:02:32 -0700

    hi

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main (String [] args){
+        int celsius = 35;
+        if (celsius == 35){
+            System.out.println("cxela");
+        }else if (celsius >=28){
+            System.out.println("normalna");
+        }else {
+            System.out.println("civa brat");
+
+
+
+
+        }
+        // switch
+        int dayOfTheWeek = 90;
+        switch (dayOfTheWeek) {
+            default:
+                System.out.println("ასეთი კვირის დღე არ არსებობს");}
+
+
+
+        int radius = 6;
+        double area = Math.Pi * radius * radius;
+
+
+        int a = 5;
+        int b = 17;
+        int c = a;
+        a = b;
+        b = c;
+
+
+        int lus = 32432;
+        if (lus % 2 == 0){
+            System.out.println("luwi");
+
+        }else {
+            System.out.println("kenti");
+        }
+
+
+        int leta = 213;
+
+        if (leta > 0){
+            System.out.println("dadebitia");
+        }else if (leta == 0 ) {
+            System.out.println(" nulia ");
+        }else {
+            System.out.println("uarkofitia");
+        }
+
+        int wona = 19834;
+        int simagle = 123;
+
+        double bmi = wona / simagle / simagle;
+
+        if (bmi < 18.5){
+            System.out.println("dzaan gamxdaria");
+        }
+
+    }
+}
